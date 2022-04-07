a3154f5be425369ae30a3140674b795c920fae4c
Chloe Rubinowicz <crubinowicz@hawk.iit.edu>
2022-04-07 10:55:05 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chloe Rubinowicz on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
